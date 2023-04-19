bc8fadf05203ee7a19c6f9c8e091a3b62ee936a0
shahrozjan <shahrozjan67@gmail.com>
2023-04-19 19:53:25 -0400

    Android Second page

---
+++ mobile/Second/Second/Second/SecondPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Second
+{	
+	public partial class SecondPage : ContentPage
+	{	
+		public SecondPage ()
+		{
+			InitializeComponent ();
+            ImgLogo1.Source = ImageSource.FromFile("Logo1");
+            ImgLogo2.Source = ImageSource.FromFile("Logo2");
+            NavigationPage.SetHasBackButton(this, false);
+            //Title = "Login";
+
+            //NavigationPage.SetHasBackButton(this, false);
+
+            //((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Color.FromHex("3071CF");
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
+
